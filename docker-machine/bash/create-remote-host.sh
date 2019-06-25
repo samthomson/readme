8cc759e0cd5a78d0efea8870e348fd97ad631fc6
Sam Thomson <samt@samt.st>
2019-06-25 08:26:22 +0200

    scripts to create a docker machine

---
+++ docker-machine/bash/create-remote-host.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+# read env var to get host details
+source .env
+
+# check env vars
+echo "we'll create a VPS called $DOCKER_HOST"
+echo "in region: $DO_DROPLET_REGION"
+echo "of size: $DO_DROPLET_SIZE"
+
+# create remote machine
+docker-machine create --driver=digitalocean --digitalocean-access-token=$DO_TOKEN --digitalocean-size=$DO_DROPLET_SIZE --digitalocean-region=$DO_DROPLET_REGION $DOCKER_HOST
