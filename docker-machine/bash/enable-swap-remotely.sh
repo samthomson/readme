0dd3cdd45d1f194c018281a37ef1c2a61c32cd0b
Sam Thomson <samt@samt.st>
2019-12-28 13:23:52 +0100

    dockermachine, enable swap, dir agnostic

--- docker-machine/bash/enable-swap-remotely.sh
+++ docker-machine/bash/enable-swap-remotely.sh
@@ -1,11 +1,15 @@
 #!bin/bash
-# this script should be run from project root
+# this script should be run from anywhere, since we define and use the `$SCRIPTPATH` var
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # read env var to get host name
-source .env
+source $SCRIPTPATH/.env
+
+echo "we'll enable swap on $DOCKER_HOST"
 
 # copy the enable swap bash script to the remote docker host (vps)
-docker-machine scp ./bash/swap.sh $DOCKER_HOST:/enable-swap-locally.sh
+docker-machine scp $SCRIPTPATH/swap.sh $DOCKER_HOST:/enable-swap-locally.sh
 
 # now remotely run that script on the remote host
 docker-machine ssh $DOCKER_HOST "bash /enable-swap-locally.sh"
